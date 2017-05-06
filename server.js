4d2774680738d51f82056fddcfb4bd1a30dbc25d
andrejarboe <andrejarboe@gmail.com>
2017-05-06 16:58:02 -0400

    Add files

--- server.js
+++ server.js
@@ -4,29 +4,31 @@
 
 var express = require('express');
 var path = require('path');
-var bodyPrser = require('body-parser');
+var bodyParser = require('body-parser');
 
 
-var index = require('.routes/index');
-var tasks = require('.routes/tasks');
+var index = require('./routes/index');
+var tasks = require('./routes/tasks');
 
-var port
+var port = 3000;
 
 var app = express();
 
 //View Engine
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engin', 'ejs');
-app.engine('html', require('ejs').rinderFile);
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 //set Static Folder
-app.use(express.static(path.join(__dirname), 'client'));
+app.use(express.static(path.join(__dirname, 'client')));
 
 //Body Parser MW
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: fasle}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', index);
 app.use('/api', tasks);
 
-app.listen(;)
+app.listen(port, function(){
+    console.log('Serverstarte on port ' +port);
+});
